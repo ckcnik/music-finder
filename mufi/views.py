38cfd75721eaaf7045ba188ecd94af512b7fe3d8
Суховерхов Денис <at10tion.angry@yandex.ua>
2015-07-18 14:30:51 +0300

    подключил pytube, сделал загрузку видео с youtube, добавил requirements

--- mufi/views.py
+++ mufi/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from pytube import YouTube
+import os
+
+
 def index(request):
     url = request.GET.get('url', '')
-    return HttpResponse(url)
+
+    yt = YouTube()
+    yt.url = url
+    video = yt.get('3gp', '144p', 'Simple')  # получаем видео в самом плохом качестве
+    video.download(os.path.abspath(__file__) + '/../tmp/video')
+    return HttpResponse(print(yt.videos))
