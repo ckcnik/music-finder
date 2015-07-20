aed15bd59edf72f5b102d4b2bb074eb03e75cab3
Суховерхов Денис <at10tion.angry@yandex.ua>
2015-07-21 00:06:15 +0300

    удалил js

--- mufi/static/mufi/js/scripts.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by denis on 20.07.15.
- */
-$( document ).ready(function() {
-    $('.table').height($(window).height());
-    $(window).resize(function () {
-        $('.table').height($(window).height());
-    });
-});
