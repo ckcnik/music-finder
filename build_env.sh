1d9498e511e3b084a1f8c3da7f6f6f25aeeffa1f
Суховерхов Денис <at10tion.angry@yandex.ua>
2015-07-24 23:43:14 +0300

    убрал из зависимостей лаганутый пайтюб

--- build_env.sh
+++ build_env.sh
@@ -7,7 +7,7 @@ mkdir ./logs
 echo $0: Installing dependencies
 source ./env/bin/activate
 export PIP_REQUIRE_VIRTUALENV=true
-./env/bin/pip install --requirement=./requirements.txt --log=./logs/build_pip_packages.log
+./env/bin/pip3.4 install --requirement=./requirements.txt --log=./logs/build_pip_packages.log
 
 echo $0: Making virtual environment relocatable
 virtualenv-3.4 --relocatable ./env
