9df7580e99a01389fb6d5925253bf7cf801b95bd
Суховерхов Денис <at10tion.angry@yandex.ua>
2015-07-29 22:27:20 +0300

    перевел запросы на гет (reverted from commit e01328894dee50c379e09358b4782f28be8fbfec)

--- mufi/views.py
+++ mufi/views.py
@@ -11,10 +11,10 @@ def index(request):
     """
     Главная страница
     """
-    form = UrlSendForm(request.GET)
+    form = UrlSendForm(request.POST)
 
     id = 0
-    if request.method == 'GET' and form.is_valid():
+    if request.method == 'POST' and form.is_valid():
         url = form.cleaned_data['url']
         time = form.cleaned_data['time_start'] if form.cleaned_data['time_start'] else 0
         duration = form.cleaned_data['duration'] if form.cleaned_data['duration'] else 15
@@ -50,8 +50,8 @@ def index(request):
 
 def checker_state(request):
     result = {'status': 'none'}
-    if request.method == 'GET':
-        video_id = request.GET['videoId']
+    if request.method == 'POST':
+        video_id = request.POST['videoId']
         video = Video.objects.get(pk=video_id)
         if video.state.name == State.SOUND_SEARCH_SUCCESS:
             results = video.result_set.all()
